3d9eb7edc6de622f1f048c8016a4d3a23e55ea3d
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-12-02 07:05:40 +0000

    Day 2

--- main.c
+++ main.c
@@ -60,9 +60,64 @@ void aoc_1b() {
   printf("Couldn't find suitable triple.\n");
 }
 
+void aoc_2a() {
+  FILE *input = fopen("input/2.txt", "r");
+  int count = 0;
+  int total = 0;
+  int c, minc, maxc, checkedCount;
+  char checked;
+
+  while (fscanf(input, "%d-%d %c: ", &minc, &maxc, &checked) > 0) {
+    checkedCount = 0;
+    while ((c = getc(input)) != '\n') {
+      if (c == checked) {
+        ++checkedCount;
+      }
+    }
+    if (checkedCount >= minc &&
+        checkedCount <= maxc) {
+      ++count;
+    }
+    ++total;
+  }
+
+  fclose(input);
+  printf("Valid: %d out of %d\n", count, total);
+}
+
+void aoc_2b() {
+  FILE *input = fopen("input/2.txt", "r");
+  int count = 0;
+  int total = 0;
+  int c, firstIndex, secondIndex, index, seenOnce;
+  char checked;
+
+  while (fscanf(input, "%d-%d %c: ", &firstIndex, &secondIndex, &checked) == 3) {
+    index = 1;
+    seenOnce = 0;
+    while ((c = getc(input)) != '\n') {
+      if ((index == firstIndex || index == secondIndex)
+          && c == checked) {
+        seenOnce = !seenOnce;
+      }
+      ++index;
+    }
+    if (seenOnce) {
+      ++count;
+    }
+    ++total;
+  }
+
+  fclose(input);
+  printf("Valid: %d out of %d\n", count, total);
+}
+
 int main() {
   printf("Day 1:\n");
   aoc_1a();
   aoc_1b();
+  printf("Day 2:\n");
+  aoc_2a();
+  aoc_2b();
   return 0;
 }
