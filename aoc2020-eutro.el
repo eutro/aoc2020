1871b626619bbc2377ad5559ca099ff63ec8edcb
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-12-09 07:31:21 +0000

    Add aoc-fetch

--- aoc2020-eutro.el
+++ aoc2020-eutro.el
@@ -6,15 +6,18 @@
 
 ;;; Code:
 
+(defconst aoc-root
+  (locate-dominating-file
+   (or load-file-name
+       (buffer-file-name))
+   "Makefile"))
+
 (defun aoc-compile-command (&rest args)
   "Get the command to compile with.
 
 ARGS are extra arguments to pass to make."
   (format "cd %s && make%s"
-          (eval-when-compile
-            (locate-dominating-file
-             load-file-name
-             "Makefile"))
+          aoc-root
           (apply #'concat
                  (mapcar (lambda (arg)
                            (concat " " arg))
@@ -33,11 +36,12 @@ Without a prefix argument, run all days."
            (aoc-compile-command "-s")
            (if day
                (concat " "
-                       (if (called-interactively-p 'interactive)
-                           (if (numberp day)
-                               (number-to-string day)
-                             (read-string "Day: "))
-                         (number-to-string day)))
+                       (number-to-string
+                        (if (called-interactively-p 'interactive)
+                            (if (numberp day)
+                                day
+                              (read-number "Day: " (nth 3 (decode-time))))
+                          day)))
              ""))))
 
 (defun aoc-compile (&optional args)
@@ -55,10 +59,42 @@ from the minibuffer."
                   args)
               "-k"))))
 
+(defun aoc-fetch (day)
+  "Fetch the input data for DAY.
+
+Requires a key file \"session.key\" in the root directory of this repo."
+  (interactive "P")
+  (let* ((day (or (and day (prefix-numeric-value day))
+                  (read-number "Day: " (nth 3 (decode-time)))))
+         (file (concat aoc-root "session.key"))
+         (session
+          (when (file-readable-p file)
+            (with-temp-buffer
+              (insert-file-contents file)
+              (buffer-substring (point-min) (point-max))))))
+    (if (not session)
+        (message "%s" "Couldn't get session.key")
+      (with-temp-buffer
+        (with-temp-message "Fetching..."
+          (shell-command (format
+                          (concat
+                           "cd %s && "
+                           "curl -fs -H\"Cookie: session=%s\" "
+                           "\"https://adventofcode.com/2020/day/%s/input\" "
+                           "> input/%s.txt "
+                           "&& printf \"Done\" || printf \"Errored!\"")
+                          aoc-root
+                          session
+                          day
+                          day)
+                         (current-buffer)))
+        (message (buffer-substring (point-min) (point-max)))))))
+
 (defconst aoc-keymap
   (let ((keys (make-sparse-keymap)))
     (define-key keys (kbd "C-c C-r") #'aoc-run)
     (define-key keys (kbd "C-c C-c") #'aoc-compile)
+    (define-key keys (kbd "C-c C-f") #'aoc-fetch)
     keys))
 
 (define-minor-mode aoc2020-eutro
